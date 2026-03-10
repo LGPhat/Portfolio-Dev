3f0e6d6033692c0a4f2a013aaf86d932848e5cb1
Phát Lê <giaphatle2103@gmail.com>
2026-03-10 10:14:35 +0700

    Add Vercel Speed Insights

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+// Gọi hàm này để kích hoạt theo dõi hiệu năng
+injectSpeedInsights();
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
